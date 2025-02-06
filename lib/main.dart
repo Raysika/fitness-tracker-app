28c65a7d15625b26ef58e545c93320677e371cfb
Raysika <raymondsillakata@gmail.com>
2025-02-06 16:49:57 +0300

    Added Supabase to my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  );
   runApp(const MyApp());
 }
 
