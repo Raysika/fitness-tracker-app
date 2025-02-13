4cf605fa8df92d95b1ea7e2fdbc07942edcc9bed
Raysika <raymondsillakata@gmail.com>
2025-02-13 09:07:40 +0300

    Added a functionality to prevent access to dashboard without login

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:fitness_tracker/screens/auth/login_screen.dart';
 import 'package:fitness_tracker/screens/dashboard/dashboard_screen.dart';
 import 'package:fitness_tracker/screens/auth/signup_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 final GoRouter appRouter = GoRouter(
   routes: [
     GoRoute(
@@ -11,8 +12,18 @@ final GoRouter appRouter = GoRouter(
     ),
     GoRoute(
       path: '/dashboard',
-      builder: (context, state) => const DashboardScreen(),
+      builder: (context, state) => DashboardScreen(),
     ),
-    GoRoute(path: '/signup', builder: (context, state) => const SignupScreen()),
+    GoRoute(
+      path: '/signup',
+      builder: (context, state) => const SignupScreen()
+    ),
+       GoRoute(
+      path: '/dashboard',
+      builder: (context, state) {
+        final user = Supabase.instance.client.auth.currentUser;
+        return user != null ? DashboardScreen() : LoginScreen();
+      },
+    ),
   ],
 );
