5990f8b0884e1e36abe26228c98d447b1006f06c
Raysika <raymondsillakata@gmail.com>
2025-03-25 15:15:06 +0300

    Made a rookie mistake and corrected it by removing my credentials for supabase and put it in an .env file

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,6 @@ import 'package:fitness_tracker/routes/router.dart';
 import 'package:fitness_tracker/themes/theme.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
-    url: 'https://nhagdzotvchrxzkoojeu.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oYWdkem90dmNocnh6a29vamV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3Njc5MjUsImV4cCI6MjA1NDM0MzkyNX0.oEgc2qQy_uAeHHo_jrzcFuJE2cm9uonsCbBnt8-I4h4',
-  );
   runApp(MyApp());
 }
 
