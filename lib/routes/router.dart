44f570919c9bcf104afb0116f5b532a6d9e5011f
Raysika <raymondsillakata@gmail.com>
2025-03-26 13:17:41 +0300

    Created Splash, onboarding, complete profile and goal selection routes as well as removed supabase import and authentication

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,12 +1,23 @@
+import 'package:fitness_tracker/screens/auth/complete_profile_screen.dart';
+import 'package:fitness_tracker/screens/auth/goal_selection_screen.dart';
+import 'package:fitness_tracker/screens/splash/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 import 'package:fitness_tracker/screens/auth/login_screen.dart';
 import 'package:fitness_tracker/screens/auth/signup_screen.dart';
 import 'package:fitness_tracker/screens/dashboard/home_screen.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:fitness_tracker/screens/onboarding/onboarding_screen.dart';
 import 'routes.dart'; // Import the routes.dart file
 
 final GoRouter appRouter = GoRouter(
   routes: [
+    GoRoute(
+      path: AppRoutes.splash,
+      builder: (context, state) => const SplashScreen(),
+    ),
+     GoRoute(
+      path: AppRoutes.onboarding,
+      builder: (context, state) => const OnboardingScreen(),
+    ),
     GoRoute(
       path: AppRoutes.login, // Use named constant
       builder: (context, state) => const LoginScreen(),
@@ -16,11 +27,21 @@ final GoRouter appRouter = GoRouter(
       builder: (context, state) => const SignupScreen(),
     ),
     GoRoute(
+      path: AppRoutes.completeProfile,
+      builder: (context, state) => const CompleteProfileScreen(),
+    ),
+     GoRoute(
+      path: AppRoutes.goalSelection,
+      builder: (context, state) => const GoalSelectionScreen(),
+    ),
+    GoRoute(
       path: AppRoutes.home, // Use named constant
-      builder: (context, state) {
-        final user = Supabase.instance.client.auth.currentUser;
-        return user != null ? HomeScreen() : LoginScreen();
-      },
+      builder: (context, state) => const HomeScreen(),
+      // {
+      //   return const HomeScreen(); // Always navigate to HomeScreen
+      //   // final user = Supabase.instance.client.auth.currentUser;
+      //   // return user != null ? HomeScreen() : LoginScreen();
+      // },
     ),
   ],
 );
