19cf44669c7b837d32c7676246d9185714599389
Raysika <raymondsillakata@gmail.com>
2025-03-26 13:12:03 +0300

    Removed unnecessary supabase_flutter import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:fitness_tracker/routes/router.dart';
 import 'package:fitness_tracker/themes/theme.dart';
 
