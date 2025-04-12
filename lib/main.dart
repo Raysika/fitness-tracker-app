ca81033bb7a7c9a7e3a34710ba9b7d300a96a474
Raysika <raymondsillakata@gmail.com>
2025-04-12 23:33:35 +0300

    Initialised supabase in my project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:fitness_tracker/routes/router.dart';
 import 'package:fitness_tracker/themes/theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(MyApp());
 }
 
